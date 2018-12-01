082399fc29d6ede8c1d9827b22a09ff2523fa4a4
SergiGomez <sergi.gomez.palleja@gmail.com>
2018-12-01 13:10:17 +0100

    convert char to int

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ import prediction
 path_data = '/Users/sergigomezpalleja/Downloads/'
 user_name_version = 'sergi_1'
 print_to_log = True
-do_preprocess = False
+do_preprocess = True
 do_modelling = False
 do_prediction = False
 do_submission = False
 
 ## Training parameters
 model_name = 'lgb'
+
+catVarsDict = {'Country' : 'LabelEncoder',
+               'Brand_Group': 'LabelEncoder',
+               'Cluster': 'LabelEncoder'}
 
 if print_to_log == True:
     todayDate = dt.datetime.today().strftime('%Y%m%d')
@@ -36,15 +40,16 @@ print('\tTraining set shape: {} Rows, {} Columns'.format(*train.shape))
 
 # Pre-processing
 if do_preprocess:
-
-    train_X = preprocessing.data_preprocessing(df = train)
+    train_X = preprocessing.data_preprocessing(df = train, catVarsDict = catVarsDict)
     print('\tShape train after Pre-processing: ', train_X.shape)
     filename_preprocessed_data = os.path.join(path_data + 'train_preprocessed_' + user_name_version + '.csv')
-    train_X.to_csv(filename_preprocessed_data)
+    train_X.to_csv(filename_preprocessed_data, index=False)
+    print("OK")
 
 else:
     #train_X = pd.read_csv(os.path.join(path_data))
     #print('\tShape of file Pre-processed: ', train_X.shape)
+    pass
 
 # Training
 if do_modelling:
--- preprocessing.py
+++ preprocessing.py
@@ -2,22 +2,18 @@ import pandas as pd
 import numpy as np
 import time
 
-def data_preprocessing(df):
+def data_preprocessing(df, catVarsDict):
 
     print('\n-- Data Pre-processing --')
     t0 = time.time()
 
-    # If data is in Wide format, we need to flatten it
-    key_vars= ['Cluster',
-                'Brand Group',
-                'Country',
-                'Function']
+    df = char_to_int(df, catVarsDict)
 
-    df = pd.melt(df[list(df.columns[-50:])+ key_vars],
-                        id_vars= key_vars, var_name='date', value_name='sales')
+    #df = pd.melt(df[list(df.columns[-50:])+ key_vars],
+    #                    id_vars= key_vars, var_name='date', value_name='sales')
 
     # Convert date to datetime format
-    #df['date'] = df['date'].astype('datetime64[ns]')
+    df['date'] = df['date'].astype('datetime64[ns]')
 
     # Identify if it's weekend
     #df['weekend'] = ((df.date.dt.dayofweek) // 5 == 1).astype(float)
@@ -29,3 +25,24 @@ def data_preprocessing(df):
 def stationarize_series(df):
 
     ads_diff = ads.Ads - ads.Ads.shift(24)
+
+def char_to_int(df, catVarsDict):
+
+    for var , value in catVarsDict.items():
+
+        if (value == 'BinaryEncoder'):
+            encoder = category_encoders.BinaryEncoder(cols = [var],
+                                            drop_invariant = True,
+                                            return_df = True)
+            df = encoder.fit_transform(df)
+        elif (value == 'LabelEncoder'):
+            df[var] = df[var].str.extract('(\d+)').astype(int)
+            #df[var] = LabelEncoder().fit_transform(df[var])
+        elif (value == 'OneHot'):
+            encoder = category_encoders.one_hot.OneHotEncoder(cols = [var],
+                                            drop_invariant = True,
+                                            return_df = True,
+                                            use_cat_names = True)
+            df = encoder.fit_transform(df)
+
+    return df
